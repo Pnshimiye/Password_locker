b286ed0fabb98e0f6315ff08e4b6a9e41a33f378
Pnshimiye <pnshimiye@gmail.com>
2019-02-11 15:58:20 +0200

    test te creation of user class

--- credentials.py
+++ credentials.py
@@ -1,33 +1,18 @@
 class Credential:
-	'''
-	Class to create  account credentials 
-	''' 
- 
-	# credentials_list = []
-  
-  user_credentials_list = []
+    '''
+    Class to create  account credentials 
+    ''' 
+    pass
 
+    Credentials_list = []
 
+    
+    def __init__(self,user_fname,website_name,login_name,password):
+        '''
+        Method to define the properties for each user object will hold. 
+        '''
 
-	@classmethod
-	def check_user(cls,first_name,password):
-		'''
-		Method that checks if the name and password entered match entries in the users_list
-		'''
-		current_user = ''
-		for user in User.users_list:
-			if (user.first_name == first_name and user.password == password):
-				current_user = user.first_name
-		return current_user
-
-	def __init__(self,user_fname,website_name,login_name,password):
-		'''
-		Method to define the properties for each user object will hold.
-		'''
-
-		# instance variables
-
-		self.user_fname = user_fname
-		self.website_name = website_name
-		self.login_name = login_name
-		self.password = password
+        self.user_fname = user_fname
+        self.website_name = website_name
+        self.login_name = login_name
+        self.password = password
--- locker_tests.py
+++ locker_tests.py
@@ -24,4 +24,38 @@ class TestUser(unittest.TestCase):
             self.assertEqual(self.new_user.first_name,"Pauline")
             self.assertEqual(self.new_user.second_name,"Nshimiye")
             self.assertEqual(self.new_user.password,"ouliouli")
-          
+
+
+    def test_save_user(self):
+            '''
+            test_save_user checks if the user instance is being saved to the user list
+            '''
+            self.new_user.save_user() 
+            self.assertEqual(len(User.users_list),1)
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.users_list = []
+
+
+    def test_save_multiple_users(self):
+            '''
+           test_save_multiple_users tests if we can save more than one users on our user list
+            '''
+            self.new_user.save_user()
+            test_user = User("Alice","Umulisa","aliumu") 
+            test_usert.save_user()
+            self.assertEqual(len(User.user_list),2)
+
+
+
+
+
+
+class TestCredentials(unittest.TestCase):
+              
+
+  if __name__ == '__main__':
+      unittest.main()
